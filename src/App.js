67530c9de029a45053bef6a7dacf0ba9c3a2dab3
IsaacThaJunior <isaacjunez669@gmail.com>
2022-03-23 14:39:31 +0100

    nivo is dope

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+import Calendar from "./components/calendar";
+import Stream from "./components/stream";
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<nav>
+				<NavLink exact activeStyle={{ textDecoration: "underline" }} to="/">
+					Stream
+				</NavLink>
+				<NavLink exact activeStyle={{ textDecoration: "underline" }} to="/calendar">
+					Calendar
+				</NavLink>
+			</nav>
+			<Switch>
+				<Route exact path="/" component={Stream} />
+				<Route exact path="/calendar" component={Calendar} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
+
+export default App;
---
+++ src/components/stream.jsx
@@ -0,0 +1,105 @@
+import { ResponsiveStream } from "@nivo/stream";
+import { streamData } from "../streamData";
+
+const Stream = () => {
+	return (
+		<div style={{ height: "400px" }}>
+			<h2>My Awesome Stream</h2>
+
+			<ResponsiveStream
+				data={streamData}
+				keys={["Ronaldo", "Neymar", "Messi"]}
+				margin={{ top: 50, right: 180, bottom: 50, left: 100 }}
+				axisTop={null}
+				axisRight={null}
+				axisBottom={{
+					orient: "bottom",
+					tickSize: 5,
+					tickPadding: 5,
+					tickRotation: 0,
+					legend: "Number of Years Playing",
+					legendOffset: 36,
+				}}
+				axisLeft={{
+					orient: "left",
+					tickSize: 5,
+					tickPadding: 5,
+					tickRotation: 0,
+					legend: "Number of Goals",
+					legendOffset: -40,
+				}}
+				offsetType="silhouette"
+				colors={{ scheme: "accent" }}
+				fillOpacity={0.85}
+				borderColor={{ theme: "background" }}
+				defs={[
+					{
+						id: "dots",
+						type: "patternDots",
+						background: "inherit",
+						color: "#ccc",
+						size: 4,
+						padding: 2,
+						stagger: true,
+					},
+					{
+						id: "squares",
+						type: "patternSquares",
+						background: "inherit",
+						color: "#e4c912",
+						size: 6,
+						padding: 2,
+						stagger: true,
+					},
+				]}
+				fill={[
+					{
+						match: {
+							id: "Paul",
+						},
+						id: "dots",
+					},
+					{
+						match: {
+							id: "Marcel",
+						},
+						id: "squares",
+					},
+				]}
+				dotSize={8}
+				dotColor={{ from: "color" }}
+				dotBorderWidth={2}
+				dotBorderColor={{ from: "color", modifiers: [["darker", 0.7]] }}
+				legends={[
+					{
+						anchor: "bottom-right",
+						direction: "column",
+						translateX: 100,
+						itemWidth: 80,
+						itemHeight: 20,
+						itemTextColor: "#999999",
+						symbolSize: 12,
+						symbolShape: "circle",
+						effects: [
+							{
+								on: "hover",
+								style: {
+									itemTextColor: "#000000",
+								},
+							},
+						],
+					},
+				]}
+			/>
+
+			<footer>
+				<p>Representing football goals stats with Nivo stream</p>
+				<p style={{ fontStyle: "oblique", marginTop: "0.5rem" }}>
+					PS: Not real stats
+				</p>
+			</footer>
+		</div>
+	);
+};
+
+export default Stream;
